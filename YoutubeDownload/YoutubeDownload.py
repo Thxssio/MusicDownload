d511cb89ea8c3e4873c00565d1d739f653f2c3b3
Thassio <95764952+Thxssio@users.noreply.github.com>
2023-04-29 01:19:24 -0300

    Add files via upload

--- YoutubeDownload/YoutubeDownload.py
+++ YoutubeDownload/YoutubeDownload.py
@@ -22,13 +22,24 @@ B = "\033[0;36m"  # blue (choice) | Color
 Y = "\033[0;33m"  # yellow (info) | Color 
 
 
-
+from PySimpleGUI import PySimpleGUI as sg
 from pytube import YouTube #import the lib
 from pytube import Playlist #import the lib
 import moviepy.editor as mp #import the lib
 import re #import the lib
 import os #import the lib
 import sys #import the lib
+
+
+sg.theme('Reddit')
+layout = [
+    [sg.Text('Link do video:'), sg.Input(key='Link', size=(20, 1))],
+    [sg.Text('Salvar em:    '), sg.Input(key='Dir', size=(20, 1))],
+    [sg.FolderBrowse(size=(30, 1), key='Browse', target='Dir')],
+    [sg.Button('Download')]
+]
+
+janela = sg.Window('Youtube Download', layout)
 
 def banner(): #def banner lib
     print(B)
@@ -46,13 +57,13 @@ def banner(): #def banner lib
     print(f"     Written by {B}Thxssio{C} (DEX)")
 
 
-def download_music():
-    link = input('Digite o Link do Vídeo: ')
-    path = input('Caminho de download: ')
+def download_music(link,path):
+    banner()
+    print(link)
+    print(path)
+
     yt = YouTube(link)
-    #Fazer o dowload
     ys = yt.streams.filter(only_audio=True).first().download(path)
-    #Converter o video(mp4) para mp3
     for file in os.listdir(path):                 
         if re.search('mp4', file):                                     
             mp4_path = os.path.join(path , file)  
@@ -61,28 +72,15 @@ def download_music():
             new_file.write_audiofile(mp3_path)    
             os.remove(mp4_path)                    
     print("Download Completo")
-
-def download_playlist():
-    Playlist = int(input('Digite a quantidade de musicas: '))
-    musicas = list()
-    path = input('Caminho de download: ')
-    for cc in range(1, Playlist + 1):
-        links = str(input(f'Digite o {cc}ª link: '))
-        musicas.append(links)
-    for musica in musicas:
-        yt = YouTube(musica)
-        ys = yt.streams.filter(only_audio=True).first().download(path)
-        for file in os.listdir(path):                 
-            if re.search('mp4', file):                                     
-                mp4_path = os.path.join(path , file)  
-                mp3_path = os.path.join(path, os.path.splitext(file)[0]+'.mp3') 
-                new_file = mp.AudioFileClip(mp4_path)  
-                new_file.write_audiofile(mp3_path)    
-                os.remove(mp4_path)  
-                print("Download Completo")      
+    
 
 
-banner()
-download_music()
-
- 
+while True:
+    eventos, valores = janela.read()
+    link = valores['Link']
+    path = valores['Dir']     
+    
+    if eventos == sg.WINDOW_CLOSED:
+        break
+    if eventos == 'Download':
+        download_music(link,path)
